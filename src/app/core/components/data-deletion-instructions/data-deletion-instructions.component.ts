234fc6662e11a46851b9e98cf22d5ed1c1793695
michacp <michacp@hotmail.com>
2025-05-28 22:54:24 -0500

    creacion de componentes para las politicas  y el banner

---
+++ src/app/core/components/data-deletion-instructions/data-deletion-instructions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-deletion-instructions',
+  imports: [],
+  templateUrl: './data-deletion-instructions.component.html',
+  styleUrl: './data-deletion-instructions.component.css'
+})
+export class DataDeletionInstructionsComponent {
+
+}
