ead2ed2fdf0f56c8b37d1cf7a435c112656ff2ca
michacp <michacp@hotmail.com>
2025-05-29 23:05:12 -0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { PrivacyPolicyComponent } from './core/components/privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [
-    { path: '', component: HomeComponent},
+export const routes: Routes = [   // Redirección de ruta vacía a 'home'
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+
+
+
+
+    { path: '**', redirectTo: 'home' }
 ];
